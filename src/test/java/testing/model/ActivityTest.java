c9982934969a3a8e554972ad7c62636671cd5e6a
gazar <kolajokimzee2020@gmail.com>
2025-12-02 13:16:07 +0100

    Activity update

--- src/test/java/testing/model/ActivityTest.java
+++ src/test/java/testing/model/ActivityTest.java
@@ -1,4 +1,28 @@
 package testing.model;
 
+import org.enums.xapi.model.Activity;
+import org.enums.xapi.model.LanguageMap;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ActivityTest {
+    @DisplayName("Activity name resolution")
+    @Test
+    public void testActivityNameResolution() {
+        Activity act = new Activity("activityOne", "watching video");
+
+        assertEquals("activityOne", act.getId());
+        assertEquals("watching video", act.getName());
+    }
+    @DisplayName("Activity definition set")
+    @Test
+    public void testActivityDefinitionSet() {
+        LanguageMap map = LanguageMap.of("en-US", "Lesson one");
+        Activity.Definition def = new Activity.Definition(map);
+        Activity act = new Activity();
+        act.setDefinition(def);
+        assertEquals("Lesson one", act.getName());
+    }
+
 }
