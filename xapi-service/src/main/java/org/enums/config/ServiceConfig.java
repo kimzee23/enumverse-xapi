0f3d6e8abffa4facc50686041021c3e6bb858194
johdanike <chinedunomeh2015@gmail.com>
2025-12-19 14:41:25 +0100

    Added the lrs service config class

--- xapi-service/src/main/java/org/enums/config/ServiceConfig.java
+++ xapi-service/src/main/java/org/enums/config/ServiceConfig.java
@@ -1,7 +1,5 @@
 package org.enums.config;
 
-package org.enums.config;
-
 import org.enums.validation.XapiValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ServiceConfig {
 
-    // This creates the "Bean" that LrsServiceImpl is looking for!
     @Bean
     public XapiValidator xapiValidator() {
         return new XapiValidator();
